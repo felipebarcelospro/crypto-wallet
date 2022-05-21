e3b19e3825a4abe6e46b3f641d2801982feffb0e
Felipe Barcelos <felipebarcelospro@MacBook-Air-de-Felipe.local>
2022-05-20 22:02:35 -0300

    feat: update

--- application/src/styles/theme.ts
+++ application/src/styles/theme.ts
@@ -1,6 +1,9 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark'
+  },
   fonts: {
     body: 'Inter, system-ui, sans-serif',
     heading: 'Inter, system-ui, sans-serif'
